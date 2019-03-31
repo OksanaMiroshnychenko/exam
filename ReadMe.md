e32dcb12b60344e88d79ce153f20316cac441075
Oksana Miroshnychenko <o_miroshnychenko@ukr.net>
2019-03-31 20:15:55 +0300

    Exam

---
+++ ReadMe.md
@@ -0,0 +1,9 @@
+### Project that runs Google simple search tests.
+### To open project must be installed on your PC:
+* Java SE Development Kit 8 - https://www.oracle.com/technetwork/java/javase/downloads/jdk8-downloads-2133151.html
+* IntelliJ IDEA - https://www.jetbrains.com/idea/download/#section=windows
+* ChromeDriver - http://chromedriver.chromium.org/downloads (copy to C:\Windows\System32)
+* Selenium Java - https://mvnrepository.com/artifact/org.seleniumhq.selenium
+### Import project to your IntelliJ IDEA.
+
+
--- src/test/java/test/SearchTests.java
+++ src/test/java/test/SearchTests.java
@@ -4,18 +4,34 @@ import org.testng.Assert;
 import org.testng.annotations.Test;
 import page.SearchResultsPage;
 
+import java.util.List;
+
 public class SearchTests extends BaseTest {
 
     @Test
-    public void SearchTest() {
+    public void caseSensitiveSearchTest() {
         String searchTerm = "Selenium";
 
         Assert.assertTrue(googleSearchHomePage.isPageLoaded(), "GoogleSearchHome page did not load.");
 
-        SearchResultsPage searchResultsPage = googleSearchHomePage.search(searchTerm);
-        Assert.assertTrue(searchResultsPage.isPageLoaded(), "SearchResults page did not load.");
+        SearchResultsPage FirstSearchResultsPage = googleSearchHomePage.search(searchTerm);
+        Assert.assertTrue(FirstSearchResultsPage.isPageLoaded(), "FirstSearchResult page did not load.");
+        Assert.assertEquals(FirstSearchResultsPage.getSearchResultsCount(), 9, "Not enough search results on search page.");
 
-        Assert.assertTrue(searchResultsPage.getSearchResultsCount() >= 9, "Not enough search results on search page");
+
+        List<String> FirstSearchResultsPageList = FirstSearchResultsPage.getSearchResultsList();
+        for (String searchResult: FirstSearchResultsPageList) {
+            Assert.assertTrue(searchResult.contains(searchTerm), "Search Term" + searchTerm + "not found in:\\n" + searchResult );
+        }
+
+        SearchResultsPage SecondSearchResultsPage = googleSearchHomePage.search(searchTerm);
+        Assert.assertTrue(SecondSearchResultsPage.isPageLoaded(), "FirstSearchResult page did not load.");
+        Assert.assertEquals(SecondSearchResultsPage.getSearchResultsCount(), 10, "Not enough search results on search page.");
+
+        List<String> SecondSearchResultsPageList = SecondSearchResultsPage.getSearchResultsList();
+        for (String searchResult: SecondSearchResultsPageList) {
+            Assert.assertTrue(searchResult.contains(searchTerm), "Search Term" + searchTerm + "not found in:\\n" + searchResult );
+        }
 
     }
 
